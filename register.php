42bd17697c0b1a2759fd1033d28ed46b97dc500f
Tmimi Haroun <haroun.tmimi@hotmail.com>
2017-01-14 23:37:23 +0100

    debut inscription

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+
+require('config.php');
+
+?>
+
+<form action="" method="post">
+	<input type="text" placeholder="Nom" name="name" id="name">
+	<input type="text" placeholder="Prenom" name="surname" id="surname">
+	<br/>
+	<input type="text" placeholder="Nom d'utilisateur" name="username" id="username">
+	<input type="password" placeholder="Mot de passe" name="password" id="password">
+	<br/>
+	<input type="submit" value="Envoyer">
+</form>
